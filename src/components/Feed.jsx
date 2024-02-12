df1210af3d3ca176475a18d6e8a8bc2e0f8fdfbf
Aditya Phasu <adityaphasu919@gmail.com>
2024-02-12 20:24:52 +0530

    Refactor Feed and SideBar components

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,35 @@
-import { Copyright } from "lucide-react";
 import { useState, useEffect } from "react";
 import SideBar from "./SideBar";
+import Videos from "./Videos";
+import { fetchFromAPI } from "@/utils/fetchFromAPI";
+import { Copyright } from "lucide-react";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  // useEffect(() => {
+  //   fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => {
+  //     setVideos(data.items);
+  //   });
+  // }, [selectedCategory]);
+
   return (
-    <div className="flex flex-col md:flex-row">
-      <div className="h-auto border-r border-r-gray-300 px-0 md:h-[92vh] md:px-2">
-        <SideBar />
-        <p className="mt-1.5 flex items-center justify-center gap-1 text-sm text-muted-foreground">
+    <div className="flex flex-col gap-4 md:flex-row md:gap-6">
+      <div className="flex h-auto flex-col border-r border-r-gray-300 px-0 md:h-[89vh]">
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <p className="mt-1.5 hidden items-center justify-center gap-1 text-sm text-muted-foreground md:flex ">
           <Copyright className="h-4 w-4" /> Copyright 2024
         </p>
+      </div>
+      <div className="h-[89vh] flex-1 overflow-y-auto  text-center sm:p-2 md:text-left">
+        <h4 className="mb-2 hidden text-2xl font-bold sm:block">
+          {selectedCategory} <span>videos</span>
+        </h4>
+        <Videos videos={videos} />
       </div>
     </div>
   );
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,18 +2,18 @@ import { categories } from "@/utils/constants";
 import { Button } from "./ui/Button";
 import { cn } from "@/lib/utils";
 
-const selectedCategory = "Music";
-
-const SideBar = () => {
+const SideBar = ({ selectedCategory, setSelectedCategory }) => {
   return (
-    <div className="sidebar flex h-auto gap-1 overflow-y-auto md:h-[95%] md:flex-col">
+    <div className="sidebar flex h-auto gap-4 overflow-y-auto px-3 py-2 md:h-[95%] md:flex-col md:py-0">
       {categories.map((category) => (
         <Button
           key={category.name}
-          variant="ghost"
+          onClick={() => setSelectedCategory(category.name)}
           className={cn(
-            "justify-start gap-6 rounded-xl",
-            selectedCategory === category.name && "bg-accent",
+            "justify-start gap-1.5 rounded-xl md:gap-5",
+            selectedCategory === category.name
+              ? "bg-black text-white dark:bg-white dark:text-black md:bg-accent md:text-black"
+              : "bg-accent hover:bg-accent hover:text-accent-foreground md:bg-transparent",
           )}
         >
           <category.icon strokeWidth={1.5} />
