25c14a96b3fc23b4b8a0824e89060b82c9c63b2c
Aditya Phasu <adityaphasu919@gmail.com>
2024-02-13 22:48:57 +0530

    Refactor VideoCard component imports and styling

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,13 +8,7 @@ import {
   demoChannel,
 } from "@/utils/constants";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardTitle,
-  CardHeader,
-} from "@/components/ui/Card";
+import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/Card";
 
 const VideoCard = ({
   video: {
@@ -22,18 +16,34 @@ const VideoCard = ({
     snippet,
   },
 }) => {
-  console.log(snippet);
   return (
-    <Card className="">
+    <Card className="border-none shadow-none">
       <Link to={videoId ? `/video/${videoId}` : demoVideo}>
-        <CardHeader className="h-auto w-auto overflow-hidden p-0 sm:h-[11.25rem] sm:w-80 md:rounded-lg">
+        <CardHeader className="h-auto w-auto overflow-hidden p-0 md:rounded-lg">
           <img
             src={snippet?.thumbnails?.high?.url}
             alt={snippet.title}
-            className="-my-[9.45%] object-cover"
+            className="-my-[9.45%] object-contain"
           />
         </CardHeader>
-        <CardContent>hifdddf</CardContent>
+        <CardContent className="h-[6.625rem] px-1.5 py-2 text-left md:p-1">
+          <Link to={videoId ? `/video/${videoId}` : demoVideo}>
+            <CardTitle className="truncate-2-lines text-base font-medium">
+              {snippet?.title || demoVideoTitle}
+            </CardTitle>
+          </Link>
+          <Link
+            to={
+              snippet?.channelId
+                ? `/channel/${snippet?.channelId}`
+                : demoChannel
+            }
+          >
+            <p className="truncate text-muted-foreground">
+              {snippet?.channelTitle || demoChannelTitle}
+            </p>
+          </Link>
+        </CardContent>
       </Link>
     </Card>
   );
