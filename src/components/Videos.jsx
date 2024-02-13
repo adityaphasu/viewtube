8db4392905452a12c08747e5b06b443b676ae782
Aditya Phasu <adityaphasu919@gmail.com>
2024-02-13 22:47:59 +0530

    Refactor Videos component to use grid layout

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,15 +3,13 @@ import VideoCard from "./VideoCard";
 
 const Videos = ({ videos }) => {
   return (
-    <div className="w-full flex-wrap justify-center gap-4 sm:flex md:justify-start">
-      {videos
-        .filter((video) => video.id.videoId)
-        .map((video, index) => (
-          <div key={index}>
-            {video.id.videoId && <VideoCard video={video} />}
-            {/* {video.id.channelId && <ChannelCard channelDetail={video} />} */}
-          </div>
-        ))}
+    <div className="w-full flex-wrap justify-center gap-4 sm:grid sm:grid-cols-2 md:justify-start lg:grid-cols-3 xl:grid-cols-4">
+      {videos.map((video, index) => (
+        <div key={index}>
+          {video.id.videoId && <VideoCard video={video} />}
+          {video.id.channelId && <ChannelCard channelDetail={video} />}
+        </div>
+      ))}
     </div>
   );
 };
