5a448f03b5318aa3c5153e203651e79123713971
Aditya Phasu <adityaphasu919@gmail.com>
2024-02-14 23:17:12 +0530

    Update Navbar component to include ViewTube logo and title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,9 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-between p-2.5">
       <div>
-        <Link to="/" className="dark:hidden">
+        <Link to="/" className="flex items-center gap-2  dark:hidden">
           <img src={Logo} alt="Logo" className="h-[2.8125rem] w-[2.8125rem]" />
+          <span className="hidden text-xl font-bold sm:block">ViewTube</span>
         </Link>
         <Link to="/" className="hidden dark:block">
           <img
